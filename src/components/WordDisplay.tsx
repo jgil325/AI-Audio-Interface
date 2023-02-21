75e5a8d6f4e272c749ab10f5818c1b03634b27e6
Jacob Gil <jacobgil325@gmail.com>
2023-02-21 17:05:14 -0500

    issues with rendering fixed

--- src/components/WordDisplay.tsx
+++ src/components/WordDisplay.tsx
@@ -4,35 +4,37 @@ import { Types } from "../components/BubbleChart/types";
 import styled from "styled-components";
 
 const d: Types.Data[] = [
-  { id: 1, name: "alternative", size: 350, fillColor: "green" },
-  { id: 2, name: "biodegradable", size: 100, fillColor: "green" },
-  { id: 3, name: "globe", size: 75, fillColor: "green" },
-  { id: 4, name: "recyclable ", size: 150, fillColor: "green" },
-  { id: 5, name: "biofuel", size: 150, fillColor: "green" },
-  { id: 6, name: "fossil reduce", size: 125, fillColor: "green" },
-  { id: 7, name: "planet", size: 230, fillColor: "green" },
-  { id: 8, name: "factory", size: 70, fillColor: "green" },
-  { id: 9, name: "toxic waste", size: 70, fillColor: "green" },
-  { id: 10, name: "life", size: 70, fillColor: "green" },
-  { id: 11, name: "pollution", size: 70, fillColor: "green" },
-  { id: 12, name: "rain forest", size: 50, fillColor: "green" },
-  { id: 13, name: "water", size: 60, fillColor: "green" },
-  { id: 14, name: "energy", size: 70, fillColor: "green" },
-  { id: 15, name: "emissions", size: 80, fillColor: "green" },
-  { id: 16, name: "renewable", size: 90, fillColor: "green" },
-  { id: 17, name: "resource", size: 100, fillColor: "green" },
-  { id: 18, name: "sustainable", size: 150, fillColor: "green" },
-  { id: 19, name: "environment", size: 150, fillColor: "green" },
+  { id: 1, name: "alternative", size: 350, fillColor: "#28B463" },
+  { id: 2, name: "biodegradable", size: 100, fillColor: "#28B463" },
+  { id: 3, name: "globe", size: 75, fillColor: "#28B463" },
+  { id: 4, name: "recyclable ", size: 150, fillColor: "#28B463" },
+  { id: 5, name: "biofuel", size: 150, fillColor: "#28B463" },
+  { id: 6, name: "fossil reduce", size: 125, fillColor: "#28B463" },
+  { id: 7, name: "planet", size: 230, fillColor: "#28B463" },
+  { id: 8, name: "factory", size: 70, fillColor: "#28B463" },
+  { id: 9, name: "toxic waste", size: 70, fillColor: "#28B463" },
+  { id: 10, name: "life", size: 70, fillColor: "#28B463" },
+  { id: 11, name: "pollution", size: 70, fillColor: "#28B463" },
+  { id: 12, name: "rain forest", size: 50, fillColor: "#28B463" },
+  { id: 13, name: "water", size: 60, fillColor: "#28B463" },
+  { id: 14, name: "energy", size: 70, fillColor: "#28B463" },
+  { id: 15, name: "emissions", size: 80, fillColor: "#28B463" },
+  { id: 16, name: "renewable", size: 90, fillColor: "#28B463" },
+  { id: 17, name: "resource", size: 100, fillColor: "#28B463" },
+  { id: 18, name: "sustainable", size: 150, fillColor: "#28B463" },
+  { id: 19, name: "environment", size: 150, fillColor: "#28B463" },
 ];
 
 const ChartContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-left: 150px;
+  //   margin-left: 150px;
 `;
 
-const ShuffleButton = styled.button``;
+const ShuffleButton = styled.button`
+  background-color: #28b463;
+`;
 
 const WordDisplay = () => {
   const [data, setData] = React.useState<Types.Data[]>(d.slice(1, 10));
