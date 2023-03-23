427c91b777b9c4a399eaa326c60d07d8bbe9bbbe
Jacob Gil <jacobgil325@gmail.com>
2023-03-23 16:37:32 -0400

    button nav testing and falling words testing, gonna merge and rebranch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,16 @@ import WordDisplay from "@/components/WordDisplay";
 import React from "react";
 import styled from "styled-components";
 import words from "@/components/Words";
+// import { FcAbout, FcBusinessman, FcCamera, FcFullTrash } from "react-icons/fc";
+
+// import Fab from "../components/FAB";
+
+// const actions = [
+//   { label: "About", icon: <FcAbout />, onClick: console.log },
+//   { label: "Profile", icon: <FcBusinessman />, onClick: console.log },
+//   { label: "Picture", icon: <FcCamera />, onClick: console.log },
+//   { label: "Trash", icon: <FcFullTrash />, onClick: console.log },
+// ];
 
 export const StyledHome = styled.div`
   display: flex;
@@ -20,6 +30,7 @@ const Home = () => {
     <StyledHome>
       <h1>Oistir</h1>
       {/* <WordDisplay /> */}
+      {/* <Fab actions={actions} /> */}
       <RainWords words={words} />
     </StyledHome>
   );
