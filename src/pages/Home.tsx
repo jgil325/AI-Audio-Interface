a57211fb186b97b60a38510b93e1a603912510b7
Jacob Gil <jacobgil325@gmail.com>
2023-04-04 16:21:52 -0400

    code formatting and cleaning

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,8 @@
 // home.tsx
 import RainWords from "@/components/RainWords";
-import WordDisplay from "@/components/WordDisplay";
 import React from "react";
 import styled from "styled-components";
 import words from "@/components/Words";
-import { FcAbout, FcBusinessman, FcCamera, FcFullTrash } from "react-icons/fc";
-import { useRouter } from "next/router";
 // import Fab from "../components/FAB";
 
 export const StyledHome = styled.div`
