36e7edf536f5accf8c00eb48deb1cd1979628113
Jacob Gil <jacobgil325@gmail.com>
2023-04-20 00:33:08 -0400

    PAUSE BUTTON WORKING

--- src/components/RainWords.tsx
+++ src/components/RainWords.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
 import Modal from "./Modal";
 
-// Empty div that serves as a container for rain of words
 const RainWordsWrapper = styled.div``;
 
 const RainAnimation = keyframes`
@@ -14,7 +13,6 @@ const RainAnimation = keyframes`
   }
 `;
 
-// WordWrapper: styled div that represents single word
 const WordWrapper = styled.div`
   position: absolute;
   z-index: 100;
@@ -24,7 +22,6 @@ const WordWrapper = styled.div`
   animation-delay: ${(props) => props.delay}s;
   cursor: pointer;
 `;
-//Cursor-pointer above is new
 
 const ButtonWrapper = styled.button`
   position: fixed;
@@ -38,35 +35,29 @@ const ButtonWrapper = styled.button`
 `;
 
 const RainWords = ({ words }) => {
-  // Array of objects that represent the state of each falling word
   const [wordStates, setWordStates] = useState<any[]>([]);
-  //Currently selected word that the user clicked on
   const [selectedWord, setSelectedWord] = useState("");
-  //Boolean flag that indicates whether the modal is currently open or not
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  //Window height + width are height and width of the browser window
   const [windowHeight, setWindowHeight] = useState<number>(0);
   const [windowWidth, setWindowWidth] = useState<number>(0);
+  const [isAnimationPlaying, setIsAnimationPlaying] = useState(true);
 
-  //Called whenever the array of words changes, and it it initializes a wordStates array with some values
   useEffect(() => {
     setWordStates(
       words.map((word: any, index: number) => ({
         top: -50,
         left: Math.random() * window.innerWidth,
         delay: index * 0.3,
-        shift: Math.random() * 100 - 50, // add a random shift value between -50 and 50
+        shift: Math.random() * 100 - 50,
       }))
     );
   }, [words]);
 
-  //Sets initial width and height
   useEffect(() => {
     setWindowHeight(window.innerHeight);
     setWindowWidth(window.innerWidth);
   }, []);
 
-  //Adds an event listener for resize event that updates height and width when the window is resized
   useEffect(() => {
     const handleResize = () => {
       setWindowHeight(window.innerHeight);
@@ -78,8 +69,6 @@ const RainWords = ({ words }) => {
     };
   }, []);
 
-  //Called when the word is clicked and sets selectedWord.
-  //Then opens up the modal by setting the isModalOpen state to true
   const handleWordClick = (word: string) => {
     setSelectedWord(word);
     setIsSidebarOpen(true);
@@ -89,21 +78,29 @@ const RainWords = ({ words }) => {
     setIsSidebarOpen(false);
   };
 
+  const handleAnimationToggle = () => {
+    setIsAnimationPlaying(!isAnimationPlaying);
+  };
+
   return (
-    //Contains a mapping of the wordStates Array to the Word Wrapper element for each falling word
     <RainWordsWrapper>
       {wordStates.map((word, index) => (
-        //Has an Onclick handler that calles handleWordClick function with the corresponding word from the words prop
         <WordWrapper
           key={index}
-          style={{ top: word.top, left: word.left + word.shift }}
           delay={word.delay}
           onClick={() => handleWordClick(words[index])}
+          style={{
+            top: word.top,
+            left: word.left + word.shift,
+            animationPlayState: isAnimationPlaying ? "running" : "paused",
+          }}
         >
           {words[index]}
         </WordWrapper>
       ))}
-
+      <ButtonWrapper onClick={handleAnimationToggle}>
+        {isAnimationPlaying ? "Pause" : "Play"}
+      </ButtonWrapper>
       {isSidebarOpen && (
         <Modal
           isOpen={isSidebarOpen}
