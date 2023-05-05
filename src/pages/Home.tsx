fb73c6a3d6b51457400cb4098e8ba330e5a6a7f8
Jacob Gil <jacobgil325@gmail.com>
2023-05-05 18:22:41 -0400

    popup working

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 // home.tsx
 import RainWords from "@/components/RainWords";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import words from "@/components/Words";
 // import Fab from "../components/FAB";
@@ -13,15 +13,69 @@ export const StyledHome = styled.div`
   height: 100vh;
   font-size: 2rem;
   text-align: center;
-  background-color: #FBFCFA;
+  background-color: #fbfcfa;
 `;
-// #FBFCFA
+
+interface PopupWrapperProps {
+  show: boolean;
+}
+
+const PopupWrapper = styled.div<PopupWrapperProps>`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  box-shadow: 0 3px 6px lightgrey;
+  padding: 20px;
+  width: 300px;
+  z-index: 1000;
+  display: ${(props) => (props.show ? "block" : "none")};
+  color: black;
+  border-radius: 10px;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  background-color: #353b48;
+  color: white;
+  font-size: 20px;
+  cursor: pointer;
+  padding: 10px;
+  border-radius: 5px;
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+`;
+
 const Home = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const visitedBefore = localStorage.getItem("visitedBefore");
+    if (!visitedBefore) {
+      setShowPopup(true);
+      localStorage.setItem("visitedBefore", "true");
+    }
+  }, []);
+
   return (
     <StyledHome>
-      {/* <h1>OiStir</h1> */}
-      {/* <WordDisplay /> */}
-      {/* <Fab actions={actions} /> */}
+      {showPopup && <Overlay />}
+      <PopupWrapper show={showPopup}>
+        <p>This is a popup that explains how the site works.</p>
+        <CloseButton onClick={() => setShowPopup(false)}> X </CloseButton>
+      </PopupWrapper>
       <RainWords words={words} />
     </StyledHome>
   );
